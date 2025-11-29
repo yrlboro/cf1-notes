bdb39e195f4a8aedc07c071bba77570813b2f774
yrlboro <yrleteboro@gmail.com>
2025-11-29 14:01:23 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Yosef's ActuNotes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
