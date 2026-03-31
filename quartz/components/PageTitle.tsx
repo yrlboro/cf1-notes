e54039cca2bf1b8922872a1b3bafd379d700edd2
yosefrlb <yrleteboro@gmail.com>
2026-03-31 22:35:57 +0700

    edit website

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot, joinSegments } from "../util/path"
+import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -7,30 +7,15 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <div class={classNames(displayClass, "page-title-container")}>
-      <img src={joinSegments(baseDir, "static/icon.png")} alt="Logo" class="page-title-logo" />
-      <h2 class={classNames(displayClass, "page-title")}>
-        <a href={baseDir}>{title}</a>
-      </h2>
-    </div>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>{title}</a>
+    </h2>
   )
 }
 
 PageTitle.css = `
-.page-title-container {
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-}
-
-.page-title-logo {
-  height: 2.5rem;
-  width: auto;
-  border-radius: 4px;
-}
-
 .page-title {
-  font-size: 1.75rem;
+  font-size: 1rem;
   margin: 0;
   font-family: var(--titleFont);
 }
